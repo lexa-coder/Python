27c6681a5087be5881c67426f280ed53cdb9ff9d
lexa-coder <76793139+lexa-coder@users.noreply.github.com>
2021-01-13 12:49:49 +0400

    Update Task_4.py

--- Task_4.py
+++ Task_4.py
@@ -2,13 +2,14 @@
 # Найдите самую большую цифру в числе.
 # Для решения используйте цикл while и арифметические операции.
 
-a = int(input())
-m = a % 10
-a = a // 10
+number = int(input('Введите пожалуйста целой положительное число: '))
+max = number % 10
+number = number // 10
 
 
-while a > 0:
-    if a % 10 > m:
-        m = a%10
-    a = a//10
-print(m)
+while number > 0:
+    if number % 10 > max:
+        max = number%10
+    number = number//10
+print(max)
+
