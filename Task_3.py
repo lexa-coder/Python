089def45757cc4a82a89bcceee6bb1c8b2460794
lexa-coder <76793139+lexa-coder@users.noreply.github.com>
2021-01-28 20:49:48 +0400

    Add files via upload

---
+++ Task_3.py
@@ -0,0 +1,25 @@
+"""
+3. Создать текстовый файл (не программно), построчно записать фамилии сотрудников и величину их окладов.
+Определить, кто из сотрудников имеет оклад менее 20 тыс., вывести фамилии этих сотрудников.
+Выполнить подсчет средней величины дохода сотрудников.
+"""
+# Create file "file_task_3.txt" (no program)
+
+# Create 15 string with number (no program):
+#
+# import random
+# i = 0
+# while i < 15:
+#     i += 1
+#     print(f'{random.uniform(1, 100000):.2f}')
+salar = []
+with open('file_task_3.txt', 'r', encoding='utf-8') as f:
+
+    for i in f:
+        name, sal = i.split()
+        salar.append(float(sal))
+
+        if float(sal) < 20000:
+            print(f'{name} - оклад меньше 20000 (оклад составляет {sal})')
+
+    print(f'\nCредняя величина дохода сотрудников = {sum(salar) / len(salar):.2f}')
---
+++ Task_7.py
@@ -0,0 +1,35 @@
+"""
+7. Создать (не программно) текстовый файл, в котором каждая строка должна содержать данные
+о фирме: название, форма собственности, выручка, издержки.
+Пример строки файла: firm_1 ООО 10000 5000.
+Необходимо построчно прочитать файл, вычислить прибыль каждой компании,
+а также среднюю прибыль. Если фирма получила убытки, в расчет средней прибыли ее не включать.
+Далее реализовать список. Он должен содержать словарь с фирмами и их прибылями,
+а также словарь со средней прибылью. Если фирма получила убытки,
+также добавить ее в словарь (со значением убытков).
+Пример списка: [{“firm_1”: 5000, “firm_2”: 3000, “firm_3”: 1000}, {“average_profit”: 2000}].
+Итоговый список сохранить в виде json-объекта в соответствующий файл.
+Пример json-объекта:
+[{"firm_1": 5000, "firm_2": 3000, "firm_3": 1000}, {"average_profit": 2000}]
+
+Подсказка: использовать менеджеры контекста.
+"""
+
+# Create file "file_task_7.txt" (no program)
+
+import json
+my_dict = {}
+profit = 0
+i = 0
+with open('file_task_7.txt', 'r', encoding='utf-8') as f:
+    for line in f:
+        name, firm, earnings, loss = line.split()
+        my_dict[name] = int(earnings) - int(loss)
+        i += 1
+        profit += my_dict[name]
+        average_profit = {'average_profit': round(profit / i)}
+
+    print(f'{my_dict}, {average_profit}')
+
+with open('file_task_7.json', 'w', encoding='utf-8') as f_json:
+    json.dump(my_dict, f_json, ensure_ascii=False)
