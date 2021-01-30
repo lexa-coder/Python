f3ca251eda20139689b142879ae6f0588b0e754d
lexa-coder <76793139+lexa-coder@users.noreply.github.com>
2021-01-31 01:36:50 +0400

    - default "r"

--- Task_5.py
+++ Task_5.py
@@ -7,7 +7,7 @@ with open('file_task_5.txt', 'w', encoding='utf-8') as f:
     file_num = (input('Введите набо чисел, разделённых пробелами: '))
     f.writelines(file_num)
 
-with open('file_task_5.txt', 'r', encoding='utf-8') as f:
+with open('file_task_5.txt', encoding='utf-8') as f:
     for i in f:
         num = i.split()
     print(f'Сумма введёных чисел в файле равна: {sum(map(int, num))}')
